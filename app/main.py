752e9aa42c96bb9746561ce40d41269a15b23f92
artkapl <arthurkaplan@gmx.at>
2025-01-17 00:02:24 +0100

    adds endpoint for DB info

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from functools import lru_cache
+from typing import Annotated
+from fastapi import Depends, FastAPI
+
+import config
 
 app = FastAPI()
+
+@lru_cache
+def get_settings():
+    return config.Settings()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/info")
+async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
+    return {
+        "app_name": settings.app_name,
+        "db_host": settings.db_host,
+        "db_port": settings.db_port,
+        "db_name": settings.db_name,
+    }

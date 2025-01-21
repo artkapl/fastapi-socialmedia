357ba26f447f5522e08d9b3243b1400ca40225c1
artkapl <arthurkaplan@gmx.at>
2025-01-21 20:44:20 +0100

    rename Posts table & corresponding routes

--- app/models.py
+++ app/models.py
@@ -2,23 +2,20 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 
-class PostBase(SQLModel):
+class Posts(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True, nullable=False)
+    content: str = Field(nullable=False)
+    published: bool = Field(default=True, nullable=False)
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False, title="Created Time in UTC")
+    updated_at: datetime | None = Field(default=None, title="Time of Last Update in UTC")
+
+
+class PostCreate(SQLModel):
     title: str = Field(index=True, nullable=False)
     content: str = Field(nullable=False)
     published: bool = Field(default=True, nullable=False)
 
 
-class PostSQL(PostBase, table=True):
-    __tablename__: str = "posts_sql"
-
-    id: int | None = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False, title="Created Time in UTC")
-    updated_at: datetime | None = Field(default=None, title="Time of Last Update in UTC")
-
-
-class PostCreate(PostBase):
-    pass
-
-
-class PostUpdate(PostBase):
+class PostUpdate(PostCreate):
     pass
--- app/routers/posts_orm.py
+++ app/routers/posts_orm.py
@@ -6,14 +6,14 @@ from fastapi import Depends, APIRouter, HTTPException, Query, Response, status
 from sqlmodel import Field, create_engine, select, SQLModel
 from app.database import SessionDep, engine
 
-from app.models import PostSQL, PostCreate, PostUpdate
+from app.models import Posts, PostCreate, PostUpdate
 
 ################################
 ###   SQLMODEL ORM QUERIES   ###
 ################################
 
 
-router = APIRouter(prefix="/sqlmodel/posts")
+router = APIRouter(prefix="/posts")
 
 
 def create_db_and_tables():
@@ -23,41 +23,40 @@ def create_db_and_tables():
 @router.on_event("startup")
 def on_startup():
     create_db_and_tables()
-    print("SQLModel: Created DB and Tables.")
 
 
-@router.get("/", response_model=list[PostSQL])
+@router.get("/", response_model=list[Posts])
 def get_posts_paginated(
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
 ):
-    posts = session.exec(select(PostSQL).offset(offset).limit(limit)).all()
+    posts = session.exec(select(Posts).offset(offset).limit(limit)).all()
     return posts
 
 
-@router.get("/{id}", response_model=PostSQL, responses={404: {"description": "Not Found"}})
+@router.get("/{id}", response_model=Posts, responses={404: {"description": "Not Found"}})
 def get_post(id: int, session: SessionDep):
-    post = session.get(PostSQL, id)
+    post = session.get(Posts, id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
     return post
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostSQL)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=Posts)
 def create_post(post: PostCreate, session: SessionDep):
     # Convert PostRequest to Post in DB
-    db_post = PostSQL.model_validate(post)
+    db_post = Posts.model_validate(post)
     # Store Post in DB
     session.add(db_post)
     session.commit()
     return db_post
 
 
-@router.put("/{id}", response_model=PostSQL, responses={404: {"description": "Not Found"}})
+@router.put("/{id}", response_model=Posts, responses={404: {"description": "Not Found"}})
 def update_post(id: int, post: PostUpdate, session: SessionDep):
     # get post by ID
-    db_post = session.get(PostSQL, id)
+    db_post = session.get(Posts, id)
     if not db_post:
         raise HTTPException(status_code=404, detail="Post not found")
     # Update post with Update values
@@ -74,7 +73,7 @@ def update_post(id: int, post: PostUpdate, session: SessionDep):
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, responses={404: {"description": "Not Found"}})
 def delete_post(id: int, session: SessionDep):
     # get post by ID
-    db_post = session.get(PostSQL, id)
+    db_post = session.get(Posts, id)
     if not db_post:
         raise HTTPException(status_code=404, detail="Post not found")
     session.delete(db_post)

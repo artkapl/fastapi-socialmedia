37c4050bc9e9d24e44a4153173dbb46b1857e9b7
artkapl <arthurkaplan@gmx.at>
2025-01-21 20:18:18 +0100

    set fields non-nullable & add descriptions

--- app/models.py
+++ app/models.py
@@ -3,17 +3,17 @@ from sqlmodel import SQLModel, Field
 
 
 class PostBase(SQLModel):
-    title: str = Field(index=True)
-    content: str = Field()
-    published: bool = Field(default=True)
+    title: str = Field(index=True, nullable=False)
+    content: str = Field(nullable=False)
+    published: bool = Field(default=True, nullable=False)
 
 
 class PostSQL(PostBase, table=True):
     __tablename__: str = "posts_sql"
 
     id: int | None = Field(default=None, primary_key=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime | None = Field(default=None)
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False, title="Created Time in UTC")
+    updated_at: datetime | None = Field(default=None, title="Time of Last Update in UTC")
 
 
 class PostCreate(PostBase):

b957160cd529ed69b7908bcf75865d6dffed36f3
artkapl <arthurkaplan@gmx.at>
2025-01-23 18:46:38 +0100

    hash user password on creation and update

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, HTTPException, Query, status
 from sqlmodel import select
 from app.database import SessionDep, commit_and_refresh
+from app.security import get_password_hash
 
 from app.models.users import UserCreate, User, UserPublic, UserUpdate
 
@@ -30,6 +31,9 @@ def get_user(id: int, session: SessionDep):
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=User)
 def create_user(user: UserCreate, session: SessionDep):
+    # Hash Password
+    hashed_pw = get_password_hash(user.password)
+    user.password = hashed_pw
     # Convert PostCreate object to Post in DB
     db_user = User.model_validate(user)
     # Store Post in DB
@@ -43,6 +47,9 @@ def update_user(user: UserUpdate, id: int, session: SessionDep):
     db_user = session.get(User, id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found!")
+    # Hash Password
+    hashed_pw = get_password_hash(user.password)
+    user.password = hashed_pw
     updated_data = user.model_dump(exclude_unset=True)
     db_user.sqlmodel_update(updated_data)
     db_user.updated_at = datetime.utcnow()

3458fcd6b42763bcae89c1260e781312e51b0077
artkapl <arthurkaplan@gmx.at>
2025-01-23 18:21:48 +0100

    add Update User route

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,14 +1,15 @@
+from datetime import datetime
 from typing import Annotated
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, HTTPException, Query, status
 from sqlmodel import select
 from app.database import SessionDep, commit_and_refresh
 
-from app.models.users import UserCreate, User
+from app.models.users import UserCreate, User, UserPublic, UserUpdate
 
 
 router = APIRouter(prefix="/users")
 
-@router.get("/", response_model=list[User])
+@router.get("/", response_model=list[UserPublic])
 def get_users_paginated(
     session: SessionDep,
     offset: int = 0,
@@ -22,6 +23,18 @@ def create_user(user: UserCreate, session: SessionDep):
     # Convert PostCreate object to Post in DB
     db_user = User.model_validate(user)
     # Store Post in DB
+    session.add(db_user)
+    commit_and_refresh(session, db_user)
+    return db_user
+
+@router.patch("/{id}", response_model=UserPublic)
+def update_user(user: UserUpdate, id: int, session: SessionDep):
+    db_user = session.get(User, id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found!")
+    updated_data = user.model_dump(exclude_unset=True)
+    db_user.sqlmodel_update(updated_data)
+    db_user.updated_at = datetime.utcnow()
     session.add(db_user)
     commit_and_refresh(session, db_user)
     return db_user

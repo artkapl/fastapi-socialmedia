9e955ca19fb9f065f2867f4658d4e190accacfc1
artkapl <arthurkaplan@gmx.at>
2025-01-25 18:35:09 +0100

    pass data dict to create_access_token

--- app/security.py
+++ app/security.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta, timezone
-from typing import Any
-from argon2 import PasswordHasher
 import jwt
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Any
+from argon2 import PasswordHasher
 
 from app.config import get_settings
 
@@ -21,19 +21,20 @@ def get_password_hash(password: str) -> str:
     return ph.hash(password)
 
 
-def create_access_token(subject: str | Any, expires_delta: timedelta | None = None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     """Create JWT access Token"""
     # Set expiration time
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    
-    try:
-        subject_string = str(subject)
-    except Exception as e:
-        raise ValueError(f"JWT Encoding Error: Subject can not be parsed into a string.")
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
 
-    to_encode = {"exp": expire, "sub": subject_string}
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    # get data to encode
+    to_encode = data.copy()
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
     return encoded_jwt
